dd5c912e1f7a1c62a61d686652ffb7a4b71da5b2
belinda <belindaatschool@gmail.com>
2022-07-03 00:56:23 +0300

    includes all tutorial Android fundamentals 08.1: Notifications (with code chalange)

--- app/src/main/java/com/example/myfirst_broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/myfirst_broadcastreceiver/MainActivity.java
@@ -2,20 +2,46 @@ package com.example.myfirst_broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.service.notification.StatusBarNotification;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private WifiReceiver wifiReceiver;
     private boolean isRegistered = false;
     private WifiManager wifiManager;
+    ////
+    private Button button_notify;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID = 0;
+    ///ALOW CANCEL AND UPDATE
+    private Button button_cancel;
+    private Button button_update;
+    private static final String ACTION_UPDATE_NOTIFICATION = "com.example.android.MyFirst_NotifyBroadcast.ACTION_UPDATE_NOTIFICATION";
+    private NotificationReceiver mReceiver = new NotificationReceiver();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +50,127 @@ public class MainActivity extends AppCompatActivity {
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         wifiReceiver = new WifiReceiver();
+
+        /////NOTIFIER CODE
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
+        createNotificationChannel();
+
+        ///ALOW CANCEL AND UPDATE
+        button_update = findViewById(R.id.update);
+        button_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateNotification();
+            }
+        });
+
+        button_cancel = findViewById(R.id.cancel);
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelNotification();
+            }
+        });
+        setNotificationButtonState(true, false, false);
+
+        registerReceiver(mReceiver,new IntentFilter(ACTION_UPDATE_NOTIFICATION));
+    }
+    private NotificationCompat.Builder getNotificationBuilder(){
+        Intent notificationIntent = new Intent(this, MainActivity.class);//explicit intent that opens this app by pressing the notification
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
+                NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text.")
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(notificationPendingIntent) //optional - adds intent to notification
+                .setAutoCancel(true);//optional - closes the notification when user taps on it.
+
+        return notifyBuilder;
+
     }
 
+    public void createNotificationChannel()    {
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >=  android.os.Build.VERSION_CODES.O) {
+            // Create a NotificationChannel
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID, "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+            //configure the notificationChannel
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+
+        }
+    }
+
+    public void sendNotification() {
+        Intent updateIntent = new Intent(ACTION_UPDATE_NOTIFICATION);
+        PendingIntent updatePendingIntent = PendingIntent.getBroadcast
+                (this, NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyBuilder.addAction(R.drawable.ic_update, "Update Notification", updatePendingIntent);
+
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+
+        setNotificationButtonState(false, true, true);
+    }
+
+    ///ALOW CANCEL AND UPDATE
+    public void cancelNotification() {
+        mNotifyManager.cancel(NOTIFICATION_ID);
+
+        setNotificationButtonState(true, false, false);
+    }
+
+    public void updateNotification() {
+        Bitmap androidImage = BitmapFactory
+                .decodeResource(getResources(),R.drawable.mascot_1);
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        //SET NOTIFICATION STYLE
+        //change the style of your notification and set the image and the title
+        notifyBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(androidImage)
+                .setBigContentTitle("Notification Updated!"));
+
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+
+        setNotificationButtonState(false, false, true);
+    }
+
+    void setNotificationButtonState(Boolean isNotifyEnabled,
+                                    Boolean isUpdateEnabled,
+                                    Boolean isCancelEnabled) {
+        button_notify.setEnabled(isNotifyEnabled);
+        button_update.setEnabled(isUpdateEnabled);
+        button_cancel.setEnabled(isCancelEnabled);
+    }
+
+    //a broadcast receiver that calls the updateNotification() method
+    // when the user taps an Update Notification action button inside the notification.
+    public class NotificationReceiver extends BroadcastReceiver {
+
+        public NotificationReceiver() {
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateNotification();
+        }
+    }
+
+
+    ///FOR DINAMIC BROADCAST RECEIVER
     @Override
     protected void onStart() {
         super.onStart();
@@ -34,6 +179,21 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(wifiReceiver, filter);
         isRegistered = true;
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        boolean isActiveNoti = false;
+        StatusBarNotification[] activeNotifications = mNotifyManager.getActiveNotifications();
+        for (StatusBarNotification noti   : activeNotifications) {
+            if(noti.getId() == NOTIFICATION_ID)
+                isActiveNoti = true;
+        }
+        if (!isActiveNoti)//case that notification was dismissed by swipe of user
+            setNotificationButtonState(true, false, false);
+    }
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -43,4 +203,10 @@ public class MainActivity extends AppCompatActivity {
             isRegistered=false;
         }
     }
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+
 }
